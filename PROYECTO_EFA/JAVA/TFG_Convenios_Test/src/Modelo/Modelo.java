f4770e7e72a9836c1d38c89b043e1843109fb72c
grndmb <proyectofinal.grn.dmb@gmail.com>
2022-04-25 18:07:31 +0200

    Formulario crear Empresa

--- PROYECTO_EFA/JAVA/TFG_Convenios_Test/src/Modelo/Modelo.java
+++ PROYECTO_EFA/JAVA/TFG_Convenios_Test/src/Modelo/Modelo.java
@@ -1,6 +1,7 @@
 package Modelo;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,24 @@ import org.hibernate.type.CurrencyType;
 
 import persistencia.Alumno;
 import persistencia.Curso;
+import persistencia.Empresa;
 import persistencia.Poblacion;
+
+
 
 public class Modelo {
 	
 
+	/**
+	 * Metodo para crear e insertar un nuevo Curso
+	 * @param sessionFactory
+	 * @param nombreCurso
+	 * @param nombreAbrev
+	 * @param clave
+	 * @param horasFCT
+	 * @param esPublico
+	 * @throws HibernateException
+	 */
 	public void crearCurso (SessionFactory sessionFactory, String nombreCurso, String nombreAbrev, int clave, int horasFCT, boolean esPublico) throws HibernateException {
 		
 		Session session = null;
@@ -52,6 +66,19 @@ public class Modelo {
 		}
 	}
 
+	/**
+	 * Metodo para crear e insertar un nuevo Alumno
+	 * @param sessionFactory
+	 * @param nif
+	 * @param nombreCompleto
+	 * @param seleccionado
+	 * @param telefono
+	 * @param correo
+	 * @param fechaNacimiento
+	 * @param codigoPostal
+	 * @param nombreCurso
+	 * @throws HibernateException
+	 */
 	public void crearAlumno (SessionFactory sessionFactory, String nif, String nombreCompleto, boolean seleccionado, int telefono, String correo, Date fechaNacimiento, int codigoPostal, String nombreCurso) throws HibernateException {
 	
 		Session session = null;
@@ -100,6 +127,14 @@ public class Modelo {
 		}
 	}
 		
+	
+	/**
+	 * Metodo Prueba para consultas
+	 * @param ciudad
+	 * @param sessionFactory
+	 * @param numeroVotos
+	 * @throws InterruptedException
+	 */
 	 public static void listar(String ciudad, SessionFactory sessionFactory, int numeroVotos) throws InterruptedException {
 	        
 		// leer todas las asignaturas
@@ -113,27 +148,82 @@ public class Modelo {
 	        
 	 }
 	 
+	 public void crearEmpresas (SessionFactory sessionFactory, String cifEmpresa, String nombreEmpresa, String direccionEmpresa, int telefono1, int telefono2, 
+			 String emailEmpresa, String faxEmpresa, String paginaWeb, String nombreGerente, String dniGerente, 
+			 String personaContacto, String cargoContacto, String fechaActualizacion, boolean organismoPublico,String observaciones, int codigoPostal) throws HibernateException{
+		 
+		 Session session = null;
+			
+			try {
+				session = sessionFactory.getCurrentSession();
+				session.beginTransaction();
+				
+				Empresa empresa = new Empresa();
+				empresa.setCifEmpresa(cifEmpresa);
+				empresa.setNombreEmpresa(nombreEmpresa);
+				empresa.setDireccionEmpresa(direccionEmpresa);
+				empresa.setTelefono1(telefono1);
+				empresa.setTelefono2(telefono2);
+				empresa.setEmailEmpresa(emailEmpresa);
+				empresa.setFaxEmpresa(faxEmpresa);
+				empresa.setPaginaWeb(paginaWeb);
+				empresa.setNombreGerente(nombreGerente);
+				empresa.setDniGerente(dniGerente);
+				empresa.setPersonaContacto(personaContacto);
+				empresa.setCargoContacto(cargoContacto);
+				
+				//Fecha Nacimiento
+				SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		        Date fechaActualizAUX = format.parse(fechaActualizacion);
+		        java.sql.Date fechaActualizacion1 = new java.sql.Date(fechaActualizAUX.getTime());
+				
+				empresa.setFechaActualizacion(fechaActualizacion1);
+				empresa.setOrganismoPublico(organismoPublico);
+				empresa.setObservaciones(observaciones);
+				
+				Query queryPoblacion = session.createQuery("FROM Poblacion WHERE codigoPostal = :codigoPostal");
+				queryPoblacion.setParameter("codigoPostal", codigoPostal);
+				Poblacion poblacion = (Poblacion) queryPoblacion.getSingleResult();
+				
+				empresa.setPoblacion(poblacion);
+				
+				
+				session.saveOrUpdate(empresa);
+				System.out.println(empresa);
+				session.getTransaction().commit();
+				
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				if(null != session) {
+					session.getTransaction().rollback();
+				}
+			}finally {
+				if(null != session) {
+					session.close();
+				}
+			}
+		 
+	 }
 	 
 	 public static void main (String [] args) {
 		 
 			SessionFactory sessionFactory = null;
 			
 		 
-		//Solo se hace una vez, al iniciar la aplicaci�n
+		//Solo se hace una vez, al iniciar la aplicaciï¿½n
 			Configuration configuration = new Configuration();
 			configuration.configure("hibernate.cfg.xml");
 			sessionFactory = configuration.buildSessionFactory();
-			
-			// Cualquier operacion debe hacerse dentro de una transaccion
-			sessionFactory.getCurrentSession().beginTransaction();
-		 
-		//Leer todas las asignaturas
-			Query query = sessionFactory.getCurrentSession().createQuery("FROM Curso");
-			ArrayList<Curso> listaCursos = (ArrayList<Curso>) query.list();
-			
-			for(Curso curso: listaCursos) {
-				System.out.println(curso.getNombreCurso());
-			}
-	 }
+					
+			Modelo helper = new Modelo();	
+			helper.crearEmpresas(sessionFactory, "1231-FIG", "INDRA", "Ronda de Toleado", 987654321, 123456789, "indra@minsait.com", "123213", "indra.com", "Angel Sevilla", "987654321", "Carlos", "Jefe SF", "12/01/2022", false, "Trabajo DAM", 13230);
 	
+	 
+	 
+	 
+	 
+	 
+	 }
 }
