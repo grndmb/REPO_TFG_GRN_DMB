72df6768263c253dbaca66796672e6f2e104b17a
grndmb <proyectofinal.grn.dmb@gmail.com>
2022-04-25 17:58:51 +0200

    //*/**/*

--- PROYECTO_EFA/SQL/BaseDatos_Convenio.sql
+++ PROYECTO_EFA/SQL/BaseDatos_Convenio.sql
@@ -156,5 +156,13 @@ CONSTRAINT FK_ANEXAR_ALUMNO FOREIGN KEY (NIF_ALUMNO) REFERENCES ALUMNO (NIF)
 ) COMMENT 'Tabla con la informacion de anexar alumno, empresa y practica';
 -- ---------------------------------------
 
+INSERT INTO POBLACION (CODIGO_POSTAL, NOMBRE, PROVINCIA) VALUES
+(13230,'Membrilla', 'Ciudad Real'),
+(13200,'Manzanares', 'Ciudad Real'),
+(13240,'La Solana', 'Ciudad Real'),
+(23400,'Ubeda', 'Jaen'),
+(2640,'Almansa', 'Albacete'),
+(13250,'Daimiel', 'Ciudad Real');
+
 SELECT * FROM ALUMNO;
 SELECT * FROM CURSO;
