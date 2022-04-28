fc7f9abe243231999a015338d2ae54804f7da4fb
grndmb <proyectofinal.grn.dmb@gmail.com>
2022-04-28 14:07:20 +0200

    ///

--- PROYECTO_EFA/JAVA/TFG_Convenios_Test/src/Controlador/Controlador.java
+++ PROYECTO_EFA/JAVA/TFG_Convenios_Test/src/Controlador/Controlador.java
@@ -60,46 +60,45 @@ public class Controlador implements ActionListener{
 			
 	    }if(e.getSource() == vista.btnAnadirAlumno) {
 			try {
-				boolean valido = false;
-				
-					do {
-						
-						//Validar dni
-						valido = this.validarDNI();
-						
-						//Variables
-						String nif = vista.txtNIFUSU.getText();
-						String nombreCompleto = vista.txtNombreCompletoUSU.getText();
-						boolean seleccionado;
+		
+				// NIF
+					String nif = vista.txtNIFUSU.getText();
 					
-						//Está o no seleccionado
-							if(vista.checkBoxSeleccionado.isSelected()) {
-								 seleccionado = true;
-							} else {
-								 seleccionado = false;
-							}
-						//Telefono
-							int telefono = Integer.parseInt(vista.txtTelefonoUSU.getText()); 
-						//Correo
-							String correo = vista.txtCorreoUSU.getText();
-				
-						//Fecha Nacimiento
-							SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-					        Date fechaNacimientoUSU = format.parse(vista.txtFechaNacimientoUSU.getText());
-					        java.sql.Date fechaNacimiento = new java.sql.Date(fechaNacimientoUSU.getTime());
-						
-					    //Codigo postal
-					        int codigoPostal = Integer.parseInt(vista.comboBoxCodigoPostalUSU.getSelectedItem().toString());
-					        String nombreCurso = vista.comboBoxNombreCursoUSU.getSelectedItem().toString();
-				       
-					    //INSERT
-					        modelo.crearAlumno(sessionFactory, nif, nombreCompleto, seleccionado, telefono, correo, fechaNacimiento, codigoPostal, nombreCurso);
-	
-						//Reset formulario
-					        this.resetFormularioNuevoAlumno();
+				// NOMBRE COMPLETO
+					String nombreCompleto = vista.txtNombreCompletoUSU.getText();
 					
-					}while (valido == false);
-				
+				// Está o no seleccionado
+					boolean seleccionado = false;
+					if (vista.checkBoxSeleccionado.isSelected()) {
+						seleccionado = true;
+					} else {
+						seleccionado = false;
+					}
+					
+				// Telefono
+					int telefono = Integer.parseInt(vista.txtTelefonoUSU.getText());
+
+				// Correo
+					String correo = vista.txtCorreoUSU.getText();
+
+				// Fecha Nacimiento
+					SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+					Date fechaNacimientoUSU = format.parse(vista.txtFechaNacimientoUSU.getText());
+					java.sql.Date fechaNacimiento = new java.sql.Date(fechaNacimientoUSU.getTime());
+
+				// Codigo postal
+					int codigoPostal = Integer.parseInt(vista.comboBoxCodigoPostalUSU.getSelectedItem().toString());
+				//Curso
+					String nombreCurso = vista.comboBoxNombreCursoUSU.getSelectedItem().toString();
+
+					
+				// INSERT
+				modelo.crearAlumno(sessionFactory, nif, nombreCompleto, seleccionado, telefono, correo, fechaNacimiento,
+						codigoPostal, nombreCurso);
+
+				// Reset formulario
+				this.resetFormularioNuevoAlumno();
+				        	
 			} catch (Exception exception) {
 				// TODO: handle exception
 				exception.printStackTrace();
@@ -168,73 +167,6 @@ public class Controlador implements ActionListener{
 				}
 				
 		    }
-		//Método validar DNI
-			public boolean validarDNI() {
-				Boolean valido = false;
-				String dni = vista.txtNIFUSU.getText();
-				String letraMayuscula = ""; //Guardaremos la letra introducida en formato mayúscula
-					
-				// Aquí excluimos cadenas distintas a 9 caracteres que debe tener un dni y también si el último caracter no es una letra
-				if(dni.length() != 9 || Character.isLetter(dni.charAt(8)) == false ) {
-					vista.lblErrorNuevoAlumno.setText("ERROR. NIF MAL FORMADO");
-					valido = false;
-				}
-
-				// Al superar la primera restricción, la letra la pasamos a mayúscula
-				letraMayuscula = (dni.substring(8)).toUpperCase();
-
-				// Por último validamos que sólo tengo 8 dígitos entre los 8 primeros caracteres y que la letra introducida es igual a la de la ecuación
-				// Llamamos a los métodos privados de la clase soloNumeros() y letraDNI()
-				if(soloNumeros(dni) == true && letraDNI(dni).equals(letraMayuscula)) {
-					valido = true;
-				}else {
-					vista.lblErrorNuevoAlumno.setText("ERROR. NIF MAL FORMADO");
-					valido = false;
-				}
-				
-				return valido;
-			}
-			
-			private boolean soloNumeros(String dni) {
-
-				int i, j = 0;
-				String numero = ""; // Es el número que se comprueba uno a uno por si hay alguna letra entre los 8 primeros dígitos
-				String miDNI = ""; // Guardamos en una cadena los números para después calcular la letra
-				String[] unoNueve = {"0","1","2","3","4","5","6","7","8","9"};
-
-				for(i = 0; i < dni.length() - 1; i++) {
-					numero = dni.substring(i, i+1);
-
-					for(j = 0; j < unoNueve.length; j++) {
-						if(numero.equals(unoNueve[j])) {
-							miDNI += unoNueve[j];
-						}
-					}
-				}
-
-				if(miDNI.length() != 8) {
-					return false;
-				}
-				else {
-					return true;
-				}
-			}
-			
-			private String letraDNI(String dni) {
-				
-				// pasar miNumero a integer
-				int miDNI = Integer.parseInt(dni.substring(0,8));
-				int resto = 0;
-				String miLetra = "";
-				String[] asignacionLetra = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
-
-				resto = miDNI % 23;
-
-				miLetra = asignacionLetra[resto];
-
-				return miLetra;
-			}
-			
 			
 		//Método para restablecer el formulario de nuevoAlumno
 			public void resetFormularioNuevoAlumno() {
@@ -246,6 +178,7 @@ public class Controlador implements ActionListener{
 					vista.txtCorreoUSU.setText("");
 					vista.comboBoxCodigoPostalUSU.setSelectedItem("");
 					vista.comboBoxNombreCursoUSU.setSelectedItem("");
+					vista.lblErrorNuevoAlumno.setText("");
 					vista.checkBoxSeleccionado.setSelected(false);
 				} catch (Exception e) {
 					e.printStackTrace();
